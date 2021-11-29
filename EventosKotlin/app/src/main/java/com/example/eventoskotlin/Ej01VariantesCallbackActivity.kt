21ec719cde346d84c7fdb5d392ad59aee1b678e0
avidaldo <avd.formacion@gmail.com>
2021-11-29 03:58:24 +0100

    mejorada explicación ej1, añadido ej4

--- EventosKotlin/app/src/main/java/com/example/eventoskotlin/Ej01VariantesCallbackActivity.kt
+++ EventosKotlin/app/src/main/java/com/example/eventoskotlin/Ej01VariantesCallbackActivity.kt
@@ -1,59 +1,100 @@
 package com.example.eventoskotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 import com.example.eventoskotlin.databinding.ActivityEj01VariantesCallbackBinding
+
+
+const val LOG_TAG = ":::"
+
+/** Función que representa un determinado comportamiento. A modo de ejemplo, sacar un Toast */
+fun accionAsociadaAEvento(mensaje: String) = Log.i(LOG_TAG, mensaje)
+
 
 
 class Ej01VariantesCallbackActivity : AppCompatActivity() {
 
-    protected lateinit var binding: ActivityEj01VariantesCallbackBinding
+    private lateinit var binding: ActivityEj01VariantesCallbackBinding
+
+
+    /** Clase interna que hereda de la interfaz OnclickListener */
+    class MyOnClickListener : View.OnClickListener {
+        override fun onClick(v: View) {
+            accionAsociadaAEvento("Hola")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEj01VariantesCallbackBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /* Listener anónimo con función anónima*/
-        binding.button11.setOnClickListener(
-            View.OnClickListener(fun(v: View?) {
+
+        /** Igual que en Java existen las clases anónimas (se utiliza este concepto ya que Java no
+        contempla objetos que no sean instancias de una clase), en Kotlin podremos hablar directamente
+        de objetos anónimos */
+        val onClickListener1 = object : View.OnClickListener {
+            override fun onClick(v: View) {
+                accionAsociadaAEvento("Hola")
+            }
+        }
+
+        /** Asignamos este listener a un botón */
+        binding.button10.setOnClickListener(onClickListener1)
+
+
+        /** Equivalente a listener mediante clase anónima en Java */
+        binding.button11.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                Toast.makeText(
+                    this@Ej01VariantesCallbackActivity,
+                    "Hola desde " + (v as Button).text,
+                    Toast.LENGTH_SHORT
+                ).show()
+                binding.textView.text = "Has pulsado " + (v as Button).text
+            }
+        }
+        )
+
+        /** Igual que sucede en Java con las interfaces funcionales
+        * (ver https://github.com/avidaldo/android-eventos-java/blob/pasando-funcion-lambda/EventosJava/app/src/main/java/com/example/eventosjava/MainActivity.java),
+        * las interfaces que tienen un único método abstracto (single abstract method interface)
+        * pueden ser sustituidas por una función lambda  */
+        binding.button12.setOnClickListener { v: View? ->
+            Toast.makeText(this, "Hola desde " + (v as Button).text, Toast.LENGTH_SHORT)
+                .show()
+            binding.textView.text = "Has pulsado " + (v as TextView).text
+            /* También se puede castear a TextView ya que es la clase padre de Button donde
+            se introduce el atributo text (Button es un tipo de TextView) */
+        }
+
+
+        /** Y en Kotlin, además de una función lambda (que sería lo normal) podría pasarse directamente
+        una función anónima, ya que Kotlin permite tratar a las funciones como objetos */
+        binding.button13.setOnClickListener(
+            fun(v: View?) {
                 Toast.makeText(this, "Hola desde " + (v as Button).text, Toast.LENGTH_SHORT)
                     .show()
                 binding.textView.text = "Has pulsado " + (v as Button).text
-            })
-        )
-
-        /* Listener anónimo que recibe lambda */
-        binding.button12.setOnClickListener(
-            View.OnClickListener { v: View? ->
-                Toast.makeText(this, "Hola desde " + (v as Button).text, Toast.LENGTH_SHORT)
-                    .show()
-                binding.textView.text = "Has pulsado " + (v as TextView).text
-                /* También se puede castear a TextView ya que es la clase padre de Button donde
-                se introduce el atributo text (Button es un tipo de TextView) */
+                /* Como ya ha sido casteado previamente, no es necesario volver a hacerlo */
             }
         )
 
-        /* Recibiendo directamente la lambda como parámetro del setOnClickListener */
-        binding.button13.setOnClickListener { v: View? ->
-            Toast.makeText(this, "Hola desde " + (v as Button).text, Toast.LENGTH_SHORT)
-                .show()
-            binding.textView.text = "Has pulsado " + v.text
-            /* Como ya ha sido casteado previamente, no es necesario volver a hacerlo */
-        }
 
-        /* El tipo del parámetro puede ser inferido por el compilador */
+        /** El tipo del parámetro puede ser inferido por el compilador */
         binding.button14.setOnClickListener { v ->
             Toast.makeText(this, "Hola desde " + (v as Button).text, Toast.LENGTH_SHORT)
                 .show()
             binding.textView.text = "Has pulsado " + v.text
         }
 
-        /* Cuando hay un solo parámetro de entrada, puede omitirse y utilizar la palabra reservada "it"  */
+        /** Cuando hay un solo parámetro de entrada, puede omitirse y utilizar la palabra reservada "it"  */
         binding.button15.setOnClickListener {
             Toast.makeText(this, "Hola desde " + (it as Button).text, Toast.LENGTH_SHORT)
                 .show()
--- EventosKotlin/app/src/main/java/com/example/eventoskotlin/Ej4Contadores2Activity.kt
+++ EventosKotlin/app/src/main/java/com/example/eventoskotlin/Ej4Contadores2Activity.kt
@@ -1,47 +1,69 @@
 package com.example.eventoskotlin
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.eventoskotlin.databinding.ActivityEj4Contadores2Binding
+import java.lang.NumberFormatException
 import kotlin.random.Random
 
 class Ej4Contadores2Activity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEj4Contadores2Binding
 
+
+    /* Utilizamos los setters de las propiedades para actualizar automáticamente la UI */
+
+    private var cuenta1 = 0
+        set(value) { field=value; binding.tvCuenta1.text = field.toString() }
+    private var cuenta2 = 0
+        set(value) {  field=value; binding.tvCuenta2.text = field.toString() }
+    private var cuentaG = 0
+        set(value) { field=value;  binding.tvCuentaGlobal.text = field.toString() }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEj4Contadores2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.button1.setOnClickListener {
-/*            incrementaContador(binding.tvCuenta1, binding.et1.text.toString().toInt())
-            incrementaContador(binding.tvCuentaGlobal)*/
-
-            //incrementaContador(binding.tvCuentaGlobal, binding.et1)
-            incrementaContador(binding.tvCuentaGlobal, null)
+            try {
+                val incremento = binding.et1.text.toString().toInt()
+                alerta(binding.tvIncremento1, false)
+                cuenta1 += incremento
+                cuentaG += incremento
+            } catch (e: NumberFormatException) {  // Si el EditText de incremento no tiene un número
+                alerta(binding.tvIncremento1, true)
+                Toast.makeText(this, R.string.no_incremento, Toast.LENGTH_SHORT).show()
+            }
         }
 
+        binding.button2.setOnClickListener {
+            try {
+                val incremento = binding.et2.text.toString().toInt()
+                alerta(binding.tvIncremento2, false)
+                cuenta2 += incremento
+                cuentaG += incremento
+            } catch (e: NumberFormatException) {
+                alerta(binding.tvIncremento2, true)
+                Toast.makeText(this, R.string.no_incremento, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.ivReset1.setOnClickListener { cuenta1 = 0 }
+        binding.ivReset2.setOnClickListener { cuenta2 = 0 }
+        binding.ivResetG.setOnClickListener { cuentaG = 0 }
 
     }
 
-    private fun incrementaContador(tvCuentaX: TextView, etX: EditText? = null) {
-        var cuentaX = tvCuentaX.text.toString().toInt()
-        //cuentaX += if (etX==null) 6 else etX.text.toString().toInt()
-        cuentaX += (etX?.text.toString() ?: "6").toInt()
-        tvCuentaX.text = "${cuentaX}"
-
+    private fun alerta(tv: TextView, alerta: Boolean) {
+        if (alerta) tv.setTextColor(Color.RED)
+        else tv.setTextColor(Color.BLACK)
     }
-
-
-    private fun incrementaContador(tvCuentaX: TextView, incremento: Int =1) {
-        var cuentaX = tvCuentaX.text.toString().toInt()
-        cuentaX += incremento
-        tvCuentaX.text = "${cuentaX}"
-    }
-
 
 }
